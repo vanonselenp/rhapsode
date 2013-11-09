f786aed1314011b4792cfb90e18690aaa50cdb8f
vanonselenp <vanonselen.p@gmail.com>
2013-11-09 19:15:34 +0200

    made it output in json, there is a problem in that it adds an unnecessary \ to escape character

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
   				 [ring/ring-core "1.2.1"]
   				 [ring/ring-jetty-adapter "1.2.1"]
   				 [compojure "1.1.6"]
-  				 [markdown-clj "0.9.33"]]
+  				 [markdown-clj "0.9.33"]
+  				 [org.clojure/data.json "0.2.3"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler rhapsode.core/app})
--- src/rhapsode/routes.clj
+++ src/rhapsode/routes.clj
@@ -1,10 +1,13 @@
 (ns rhapsode.routes
-	(:use markdown.core))
+	(:use markdown.core)
+	(:require [clojure.data.json :as json]))
 
 (def directory (clojure.java.io/file "/Users/petervanonselen/Development/rhapsode/resources/posts/"))
 
 (defn latest []
-	(md-to-html-string (slurp (str (.getPath directory) "/" (last (seq (.list directory)))))))
+	(json/write-str 
+		{:title "This is the title"
+		 :content (md-to-html-string (slurp (str (.getPath directory) "/" (last (seq (.list directory))))))}))
 
 (defn get-article [name]
 	(slurp (str (.getPath directory) "/" name ".md")))
