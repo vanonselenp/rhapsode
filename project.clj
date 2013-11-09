4243fc72ee812ea07edd9d2a4d40247ea34ebc5b
vanonselenp <vanonselen.p@gmail.com>
2013-11-09 07:18:43 +0200

    included compojure

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
   				 [ring/ring-core "1.2.1"]
-  				 [ring/ring-jetty-adapter "1.2.1"]])
+  				 [ring/ring-jetty-adapter "1.2.1"]
+  				 [compojure "1.1.6"]])
